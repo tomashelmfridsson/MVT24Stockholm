7e389972deffe3d07fb8a324b1d5a2cb52bfc6c1
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2024-10-30 10:40:40 +0100

    Backwards 76 och 77

---
+++ src/array/Backwards.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+// 76.En array med 10 inlästa tal finns. Lägg över dessa tal i en annan array så att talen
+// kommer i omvänd ordning.
+
+//77.Skapa ett program som läser in 10 heltal till en array
+// och sedan skriver ut talen baklänges.
+public class Backwards {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 1000);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int[] arrayBackwards = new int[10];
+        for (int j = array.length - 1; j >= 0; j--) {
+            // System.out.println(array[j]);
+            arrayBackwards[array.length-1-j] = array[j];
+        }
+        System.out.println(Arrays.toString(arrayBackwards));
+    }
+
+}
