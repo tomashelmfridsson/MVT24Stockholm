f9ff240df8ec1bab94a2fc3d06b5234f094d4f80
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2024-11-18 11:34:45 +0100

    user complete

--- src/user/UserEmail.java
+++ src/user/UserEmail.java
@@ -7,25 +7,58 @@ public class UserEmail {
     private String[] emailArray = new String[4];
     private int numberOfEmails = 0;
 
+    public int getNumberOfEmails() {
+        return numberOfEmails;
+    }
+
     public void addEmail(String email) {
-        if (numberOfEmails >= 4) {
-            emailArray[0] = emailArray[1];
-            emailArray[1] = emailArray[2];
-            emailArray[2] = emailArray[3];
-            emailArray[3] = email;
-        } else{
-            for (int i = 0; i < emailArray.length; i++) {
-                if (emailArray[i] == null) {
-                    emailArray[i] = email;
-                    numberOfEmails++;
-                    break;
+        if (email.contains("@") && (alreadyExists(email) == -1)) {
+
+            if (numberOfEmails >= 4) {
+                emailArray[0] = emailArray[1];
+                emailArray[1] = emailArray[2];
+                emailArray[2] = emailArray[3];
+                emailArray[3] = email;
+
+//            for (int i =0; i<emailArray.length-1;i++){
+//                emailArray[i] = emailArray[i+1];
+//            }
+//            emailArray[emailArray.length-1] = email;
+
+            } else {
+                for (int i = 0; i < emailArray.length; i++) {
+                    if (emailArray[i] == null) {
+                        emailArray[i] = email;
+                        numberOfEmails++;
+                        break;
+                    }
                 }
             }
         }
     }
 
     public String getEmailAtPosition(int i) {
-        System.out.println(Arrays.toString(emailArray));
         return emailArray[i];
+    }
+
+    private int alreadyExists(String email) {
+        for (int i = 0; i < numberOfEmails; i++) {
+            if (emailArray[i].equals(email)) return i;
+        }
+        return -1;
+    }
+
+    public void removeEmail(String email) {
+        // Först leta reda på index i Arrayen
+        // Ta bort emapostadrees vid indexet
+        // Flytta alla senare ett steg i arrayen
+        System.out.println(Arrays.toString(emailArray));
+        int index = alreadyExists(email);
+        for (int i = index; i < emailArray.length - 1; i++) {
+            emailArray[i] = emailArray[i + 1];
+        }
+        emailArray[3] = null;
+        numberOfEmails--;
+        System.out.println(Arrays.toString(emailArray));
     }
 }
--- src/user/UserEmailTest.java
+++ src/user/UserEmailTest.java
@@ -11,6 +11,7 @@ public class UserEmailTest {
         String email= "test@abc.se";
         userEmail.addEmail(email);
         assertEquals(email,userEmail.getEmailAtPosition(0));
+        assertEquals(1, userEmail.getNumberOfEmails());
     }
 
     @Test
@@ -22,6 +23,7 @@ public class UserEmailTest {
         userEmail.addEmail(email1);
         assertEquals(email0,userEmail.getEmailAtPosition(0));
         assertEquals(email1,userEmail.getEmailAtPosition(1));
+        assertEquals(2, userEmail.getNumberOfEmails());
     }
 
     @Test
@@ -41,6 +43,48 @@ public class UserEmailTest {
         assertEquals(email2,userEmail.getEmailAtPosition(1));
         assertEquals(email3,userEmail.getEmailAtPosition(2));
         assertEquals(email4,userEmail.getEmailAtPosition(3));
+        assertEquals(4, userEmail.getNumberOfEmails());
+    }
+
+    @Test
+    public void addEmailWithoutAt(){
+        UserEmail userEmail = new UserEmail();
+        String email= "testabc.se";
+        userEmail.addEmail(email);
+        assertEquals(null,userEmail.getEmailAtPosition(0));
+        assertEquals(0, userEmail.getNumberOfEmails());
+    }
+
+    @Test
+    public void addTwoSimiliarEmails(){
+        UserEmail userEmail = new UserEmail();
+        String email= "test@abc.se";
+        userEmail.addEmail(email);
+        userEmail.addEmail(email);
+        assertEquals(email,userEmail.getEmailAtPosition(0));
+        assertEquals(null,userEmail.getEmailAtPosition(1));
+        assertEquals(null,userEmail.getEmailAtPosition(2));
+        assertEquals(null,userEmail.getEmailAtPosition(3));
+        assertEquals(1, userEmail.getNumberOfEmails());
+    }
+
+    @Test
+    public void removeEmail(){
+        UserEmail userEmail = new UserEmail();
+        String email0= "test0@abc.se";
+        String email1= "test1@abc.se";
+        String email2= "test2@abc.se";
+        String email3= "test3@abc.se";
+        userEmail.addEmail(email0);
+        userEmail.addEmail(email1);
+        userEmail.addEmail(email2);
+        userEmail.addEmail(email3);
+        userEmail.removeEmail(email1);
+        assertEquals(email0,userEmail.getEmailAtPosition(0));
+        assertEquals(email2,userEmail.getEmailAtPosition(1));
+        assertEquals(email3,userEmail.getEmailAtPosition(2));
+        assertEquals(null,userEmail.getEmailAtPosition(3));
+        assertEquals(3, userEmail.getNumberOfEmails());
     }
 
 }
