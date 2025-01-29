c99deb78e09ab9805b40ad35075ab23b9ed6099f
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-29 11:59:05 +0100

    integerArray part3

--- src/automation/integerarray/IntegerArray.java
+++ src/automation/integerarray/IntegerArray.java
@@ -8,30 +8,58 @@ public class IntegerArray {
     // Se till att inläsningen pågår tills det att
     // användaren har fyllt i fem heltal.
     // Användaren ska få ett tydligt felmeddelande om den skriver fel.
+
+    // Låt användaren välja vilket index de vill se av heltalen.
+    // Hantera ArrayIndexOutOfBoundsException.
+    // Användaren ska kunna använda denna funktionalitet i all oändlighet.
     private static int[] integerArray = new int[5];
     private static int index = 0;
-
+    private static Scanner scanner = new Scanner(System.in);
+    private static int nfeFails=0;
+    private static int aiobFails =0;
     public static void main(String[] args) {
         while (index < 5) {
-            Scanner scanner = new Scanner(System.in);
             System.out.print("Skriv in ett heltal: ");
             String heltalString = scanner.nextLine();
             System.out.println(addArray(heltalString));
         }
         System.out.println(Arrays.toString(integerArray));
+        System.out.println(nfeFails);
+        System.out.println(aiobFails);
+        while (true) {
+            System.out.println("Vilket cell i arrayn vill du se på");
+            String cellString = scanner.nextLine();
+            System.out.println(viewArrayCell(cellString));
+        }
     }
+
+    public static String viewArrayCell(String cellString) {
+        try {
+            return Integer.toString(integerArray[Integer.parseInt(cellString)]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Du måste ange ett värde mellan 0 och 4";
+        }
+    }
+
 
     public static String addArray(String input) {
         try {
             integerArray[index] = Integer.parseInt(input);
             index++;
-            return "Heltal "+input+" tillagt i cell "+(index-1);
+            return "Heltal " + input + " tillagt i cell " + (index - 1);
         } catch (NumberFormatException e) {
-            return "Det gick inte bra att lägga till "+input+" då det inte är ett heltal, försök igen";
-        } catch (ArrayIndexOutOfBoundsException e){
+            nfeFails++;
+            return "Det gick inte bra att lägga till " + input + " då det inte är ett heltal, försök igen";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            aiobFails++;
             return "Arrayen är full";
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Något annat gick fel";
         }
+    }
+
+    public static void reset() {
+        integerArray = new int[5];
+        index = 0;
     }
 }
--- src/automation/integerarray/IntegerArrayTest.java
+++ src/automation/integerarray/IntegerArrayTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IntegerArrayTest {
     @Test
     public void testOfAddArray(){
+
         assertEquals("Heltal 5 tillagt i cell 0",IntegerArray.addArray("5"));
         assertEquals("Heltal 7 tillagt i cell 1",IntegerArray.addArray("7"));
         assertEquals("Det gick inte bra att lägga till fem då det inte är ett heltal, försök igen",
@@ -18,5 +19,14 @@ public class IntegerArrayTest {
         assertEquals("Heltal 9 tillagt i cell 3",IntegerArray.addArray("9"));
         assertEquals("Heltal 10 tillagt i cell 4",IntegerArray.addArray("10"));
         assertEquals("Arrayen är full",IntegerArray.addArray("11"));
+    }
+
+    @Test
+    public void testOfViewArrayCell(){
+        IntegerArray.reset();
+        IntegerArray.addArray("5");
+        assertEquals("5",IntegerArray.viewArrayCell("0"));
+        assertEquals("Du måste ange ett värde mellan 0 och 4",
+                IntegerArray.viewArrayCell("9"));
     }
 }
